29ddf1457c50f4f6537e4b2f58d332921b4b726d
Morgan Byrne <morganlbyrne@gmail.com>
2021-10-20 22:07:17 -0400

    edited text size in paragraphs and on the photos

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-// const h4 = document.createElement("h4");
-// h4.textContent = "This website created by Morgan Byrne";
-
-// document.querySelector("body").appendChild(h4);
-
-
-// <script type="text/javascript">
-//   writeRandomQuote = function () {
-//     var quotes = new Array();
-//     quotes[0] = "'Do or do not. There is no try.' ~Yoda";
-//     quotes[1] = "'There is no fate but what we make.' ~Kyle Reese from Terminator";
-//     quotes[3] = "'When you become comfortable with uncertainty, infinite possibilities open up in your life.' ~Eckhart Tolle";
-//     var rand = Math.floor(Math.random()*quotes.length);
-//     document.write(quotes[rand]);
-//   }
-//   writeRandomQuote();
-// </script>
 
 
